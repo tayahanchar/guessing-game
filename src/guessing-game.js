63059f156a2c4f6c36073222f13a52c7f73abb40
Taya Hanchar <tayahanchar@gmail.com>
2023-01-31 20:44:27 +0200

    feat: implement guessing-game task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,30 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.middle = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        let array = [];
+        for (let i = this.min; i < this.max; i++) {
+            array.push(i);
+        }
+        this.middle = array[Math.floor((array.length + 1) / 2)];
+        return this.middle;
     }
 
     lower() {
-
+        this.max = this.middle;
     }
 
     greater() {
-
+        this.min = this.middle;
     }
 }
 
